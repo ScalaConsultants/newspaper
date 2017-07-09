1174efd59cea3a48fb74efbd710384f3182514f4
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2017-07-09 19:51:14 +0200

    better migrations; using connection pool;

--- user-mgmt/project/Dependencies.scala
+++ user-mgmt/project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val akkaVersion = "2.5.2"
   lazy val akkaDependencies = Seq(
     "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-    "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+    "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
     "com.typesafe.akka" %% "akka-stream" % akkaVersion,
     "com.typesafe.akka" %% "akka-stream-kafka" % "0.14",
     "com.typesafe" % "config" % "1.3.1"
@@ -15,9 +15,10 @@ object Dependencies {
     "org.postgresql" % "postgresql" % "42.1.1",
     "com.typesafe.slick" %% "slick" % slickVersion,
     "org.slf4j" % "slf4j-nop" % "1.6.4",
-    "com.typesafe.slick" %% "slick-hikaricp" % slickVersion
+    "com.typesafe.slick" %% "slick-hikaricp" % slickVersion,
+    "org.flywaydb" % "flyway-core" % "4.2.0"
   )
 
   lazy val schemaDependency = "io.scalac" %% "newspaper-schema" % "0.1.0-SNAPSHOT"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1" % Test
 }
--- user-mgmt/project/plugins.sbt
+++ user-mgmt/project/plugins.sbt
@@ -1,3 +1,1 @@
-addSbtPlugin("org.flywaydb" % "flyway-sbt" % "4.2.0")
-
 resolvers += "Flyway" at "https://flywaydb.org/repo"
